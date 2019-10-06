9bd27bd76a5652a231e38a8e0dea1a95bbd1e2e6
ArihitoMatsuda <arihito.m@gmail.com>
2019-10-06 21:38:59 +0900

    edit index

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,4 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $date = new DateTime();
 $message = $date->format("今はY年m月d日 H時i分s秒です");
-echo "<h1>" . h($message) . "</h1>";
+echo "<!DOCTYPE html>\n";
+echo "<title>test</title>\n";
+echo "<h1>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
+echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
