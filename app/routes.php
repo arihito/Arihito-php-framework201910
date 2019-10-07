90f76f24c1921b9416bf3c5b153b49a1fb7d7587
ArihitoMatsuda <arihito.m@gmail.com>
2019-10-07 20:15:39 +0900

    src/index.phpとapp/routes.phpを分割

--- app/routes.php
+++ app/routes.php
@@ -1,1 +1,19 @@
 <?php
+
+$routes = [];
+
+$routes['/'] = function () {
+	$date = new DateTime();
+	$message = $date->format("今はY年m月d日 H時i分s秒です");
+	echo "<!DOCTYPE html>\n";
+	echo "<title>test</title>\n";
+	echo "<h1>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
+	echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+	exit;
+};
+
+$routes['/phpinfo.php'] = function () {
+	phpinfo();
+};
+
+return $routes;
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,21 +1,7 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$routes = [];
-
-$routes['/'] = function () {
-	$date = new DateTime();
-	$message = $date->format("今はY年m月d日 H時i分s秒です");
-	echo "<!DOCTYPE html>\n";
-	echo "<title>test</title>\n";
-	echo "<h1>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-	echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-	exit;
-};
-
-$routes['/phpinfo.php'] = function () {
-	phpinfo();
-};
+$routes = require __DIR__ . '/../app/routes.php';
 
 if (isset($routes[$_SERVER['REQUEST_URI']])) {
 	$f = $routes[$_SERVER['REQUEST_URI']];
