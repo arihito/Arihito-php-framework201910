49b3a5080d71371387185567b9dc2c16fb673bd3
ArihitoMatsuda <arihito.m@gmail.com>
2019-10-07 20:12:52 +0900

    edit routes

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,9 +1,26 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$date = new DateTime();
-$message = $date->format("今はY年m月d日 H時i分s秒です");
-echo "<!DOCTYPE html>\n";
-echo "<title>test</title>\n";
-echo "<h1>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+$routes = [];
+
+$routes['/'] = function () {
+	$date = new DateTime();
+	$message = $date->format("今はY年m月d日 H時i分s秒です");
+	echo "<!DOCTYPE html>\n";
+	echo "<title>test</title>\n";
+	echo "<h1>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
+	echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+	exit;
+};
+
+$routes['/phpinfo.php'] = function () {
+	phpinfo();
+};
+
+if (isset($routes[$_SERVER['REQUEST_URI']])) {
+	$f = $routes[$_SERVER['REQUEST_URI']];
+	$f();
+} else {
+	// http_response_code(response_code:404);
+	echo "<h1>404 Not Found</h1>";
+}
