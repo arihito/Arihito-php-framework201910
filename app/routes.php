85383e7706143fa0fca6cc56bfb760a43a9208ad
ArihitoMatsuda <arihito.m@gmail.com>
2019-10-07 20:47:47 +0900

    routes.phpの関数は甘楽図status,headers,bodyの３つを必ず返すようにした

--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,19 @@
 <?php
-
+// ①ルーティング
+// $routesにクエリ文字の連想配列内に
+// コールバック関数でViewへのアドレスを読み込んでいる。
 $routes = [];
 
+// 文字列を返す規則立てたルーティング
 $routes['/'] = function () {
+	ob_start();	// html出力をバッファに貯める
 	include __DIR__ . '/../app/view/index.phtml';
+	// 一気に返す
+	return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 $routes['/phpinfo.php'] = function () {
+	ob_start();
 	phpinfo();
 };
 
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,14 +1,25 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+// routes.phpで返されたViewルートを取得
 $routes = require __DIR__ . '/../app/routes.php';
 
+// ？以降のGETパラメータを読み込めるように変換処理
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+// もしURLにパラメータがあればコールバック関数を実行する
 if (isset($routes[$_SERVER['REQUEST_URI']])) {
 	$f = $routes[$request_uri];
-	$f();
+	[$status, $headers, $body] = $f();
+	http_response_code($status);
+	foreach ($headers as $name => $h) {
+		header("{$name}: {$h}");
+	}
+	echo $body;
+
 } else {
+
+// 適切なパラメータでなければエラー
 	http_response_code(404);
 	echo "<h1>404 Not Found</h1>";
 }
