eb8766067b0e75713f88f902a7da0ce62805da48
ArihitoMatsuda <arihito.m@gmail.com>
2019-10-07 20:32:03 +0900

    /のHTMLの生成をindex.phtmlに分割した

--- app/routes.php
+++ app/routes.php
@@ -3,13 +3,7 @@
 $routes = [];
 
 $routes['/'] = function () {
-	$date = new DateTime();
-	$message = $date->format("今はY年m月d日 H時i分s秒です");
-	echo "<!DOCTYPE html>\n";
-	echo "<title>test</title>\n";
-	echo "<h1>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-	echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-	exit;
+	include __DIR__ . '/../app/view/index.phtml';
 };
 
 $routes['/phpinfo.php'] = function () {
--- htdocs/index.php
+++ htdocs/index.php
@@ -3,10 +3,12 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $routes = require __DIR__ . '/../app/routes.php';
 
+$request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
 if (isset($routes[$_SERVER['REQUEST_URI']])) {
-	$f = $routes[$_SERVER['REQUEST_URI']];
+	$f = $routes[$request_uri];
 	$f();
 } else {
-	// http_response_code(response_code:404);
+	http_response_code(404);
 	echo "<h1>404 Not Found</h1>";
 }
