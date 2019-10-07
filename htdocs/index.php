389db10892ccabddfbe60ebc035fecdf0d0b2cc9
ArihitoMatsuda <arihito.m@gmail.com>
2019-10-07 21:07:56 +0900

    add bootstrap4

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,19 +7,16 @@ $routes = require __DIR__ . '/../app/routes.php';
 // ？以降のGETパラメータを読み込めるように変換処理
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+// 適切なパラメータが無い場合のエラー処理
+$not_found = function () {
+	return [404, ['Content-Type'=>'text/html'], "<h1>404 Not Found</h1>"];
+};
+
 // もしURLにパラメータがあればコールバック関数を実行する
-if (isset($routes[$_SERVER['REQUEST_URI']])) {
-	$f = $routes[$request_uri];
-	[$status, $headers, $body] = $f();
-	http_response_code($status);
-	foreach ($headers as $name => $h) {
-		header("{$name}: {$h}");
-	}
-	echo $body;
-
-} else {
-
-// 適切なパラメータでなければエラー
-	http_response_code(404);
-	echo "<h1>404 Not Found</h1>";
+$f = $routes[$request_uri] ?? $not_found;
+[$status, $headers, $body] = $f();
+http_response_code($status);
+foreach ($headers as $name => $h) {
+	header("{$name}: {$h}");
 }
+echo $body;
